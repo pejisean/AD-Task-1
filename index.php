91456df0bf23e4f4f429542d1d9f0faad41ceb3c
pejisean <202311735@fit.edu.ph>
2025-05-07 12:27:56 +0800

    feat - added weather forecast logic

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Weather Forecast</title>
 </head>
 <body>
-    
+    <?php
+        //Weather Forecast Logic
+        $weekdays = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+        $temperatures = array(28, 22, 15, 30, 18, 25, 20);
+
+        for($i = 0;$i< count($weekdays);$i++){
+            echo "<strong>".$weekdays[$i]."</strong>".$temperatures[$i]."<br>";
+        }
+
+    ?>
 </body>
 </html>
