db63b6d79568886c7c04b274628e1aef4b09253e
pejisean <202311735@fit.edu.ph>
2025-05-11 22:16:24 +0800

    feat - linking to tailwind cdn

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="/assets/css/example.css">
 </head>
 <body>
-    <h1>Welcome to the Weather Report Page!</h1>
-    <a href="page/index.php" class="btn">Here's the upcoming forecast!</a>
+    <div class="columns-1">
+        <h1>Welcome to the Weather Report Page!</h1>
+        <button href="page/index.php" class="btn">Here's the upcoming forecast!</button>
+    </div>
 </body>
 </html>
--- page/index.php
+++ page/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weather Forecast</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="/assets/css/example.css">
 </head>
 <body>
