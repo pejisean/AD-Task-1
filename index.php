5e908e61a83b2e7b84182eb77fd1ce3b738f63de
pejisean <202311735@fit.edu.ph>
2025-05-11 23:19:10 +0800

    feat - rain effect to background of landing page

--- index.php
+++ index.php
@@ -4,15 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="/assets/css/example.css">
 </head>
 <body>
+    <div class="rain front-row">
+    </div>
     <div class="h-screen flex item-center justify-center">
         <h1 class="font-bold text-4x1">Welcome to the Weather Report Page!</h1>
+        <a href="page/index.php" class="btn">Here's the upcoming forecast!</a>
     </div>
-    <div>
-        <button href="page/index.php" class="btn">Here's the upcoming forecast!</button>
-    </div>
+
+    <!--Javascript-->
+    <script src="/assets/js/example.js"></script>
 </body>
 </html>
