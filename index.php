7522ddb011b4056ff95d493e9806c394c44519c2
pejisean <202311735@fit.edu.ph>
2025-05-12 11:56:42 +0800

    style - retouched the landing page to look more appealing

--- index.php
+++ index.php
@@ -9,11 +9,18 @@
     <link rel="stylesheet" href="/assets/css/example.css">
 </head>
 <body>
-    <div class="rain front-row">
+    <div class="rain front-row pointer-events-none">
     </div>
-    <div class="h-screen flex item-center justify-center bg-opacity-50">
-        <h1 class="font-bold text-4x1">Welcome to the Weather Report Page!</h1>
-        <a href="page/index.php" class="btn">Here's the upcoming forecast!</a>
+    <div class="container mx-auto h-screen flex flex-col items-center justify-center bg-black-900 bg-opacity-80 rounded-lg p-8 space-y-6 text-center max-w-4x1">
+        <h1 class="fixed top-0 left-0 font-extrabold w-full text-center text-[70px] text-white leading-tight p-4 z-50">
+            Welcome to the Weather Report Page!
+        </h1>
+        <p class="text-gray-300 text-[35px] max-w-3xl mx-auto ">
+            See the upcoming weather for the week by clicking the button below!
+        </p>
+        <a href="page/index.php" class="bg-slate-400 hover:bg-slate-500 font-bold py-2 px-2 inline-block text-center rounded">
+            Click to see the upcoming forecast!
+        </a>
     </div>
 
     <!--Javascript-->
